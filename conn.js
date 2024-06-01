fa05c2f61893210ff033358f71ae5b053d3ba77b
Tahacollege <tcollegewala30@gmail.com>
2024-06-01 08:28:20 +0530

    ejsinstall

--- conn.js
+++ conn.js
@@ -1,5 +1,5 @@
 const {MongoClient}=require('mongodb')
-const url='mongodb://0.0.0.0:27017'
+//const url='mongodb://0.0.0.0:27017'
 const client=new MongoClient(process.env.MONGO_URI)
 async function connection(){
     var result=await client.connect()
