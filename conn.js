747fc7a73609a227c37a52a08a308028875f2596
Tahacollege <tcollegewala30@gmail.com>
2024-06-19 08:23:26 +0530

    eds

--- conn.js
+++ conn.js
@@ -1,7 +1,7 @@
 const {MongoClient}=require('mongodb')
 //const url='mongodb://0.0.0.0:27017'
 //const client=new MongoClient(url)
-const client=new MongoClient(process.env.MONGO_URI)
+const client=new MongoClient('mongodb+srv://tcollegewala30:fOcy87YhffoTWgnJ@cluster0.qqtdpgf.mongodb.net/b.net/')
 async function connection(){
     var result=await client.connect()
     var db=result.db('Bags')
